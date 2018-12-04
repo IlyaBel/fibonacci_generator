cdd3a8a0ca804ca11f0a39da230b60aa5c123d10
Ilya Belyavtsev <ilya.belyavtsev@solarwinds.com>
2018-12-04 19:16:03 +0300

    Add iterator for Fibonacci generator

--- fibonacci_generator.h
+++ fibonacci_generator.h
@@ -1,9 +1,50 @@
-#ifndef FIB_GENERATOR_H
-#define FIB_GENERATOR_H
+#ifndef FIBONACCI_GENERATOR_H
+#define FIBONACCI_GENERATOR_H
 
 #include <functional>
 
 template <class T, class Plus = std::plus<T>>
+class FibonacciIterator;
+
+template <class T, class Plus = std::plus<T>>
+class FibonacciGenerator;
+
+template <class T, class Plus>
+class FibonacciIterator {
+private:
+    FibonacciGenerator<T, Plus>& gen_;
+    T saved_state;
+
+public:
+    FibonacciIterator(FibonacciGenerator<T, Plus>& gen) :
+        gen_(gen), saved_state(gen.first_) {}
+
+    T operator * () const {
+        return saved_state;
+    }
+
+    auto& operator ++ () {
+        gen_();
+        saved_state = gen_.first_;
+        return *this;
+    }
+
+    auto operator ++ (int) {
+        auto tmp {*this};
+        operator++();
+        return tmp;
+    }
+
+    bool operator == (const FibonacciIterator& rhs) const{
+        return saved_state == rhs.saved_state;
+    }
+
+    bool operator != (const FibonacciIterator& rhs) const{
+        return !(*this == rhs);
+    }
+};
+
+template <class T, class Plus>
 class FibonacciGenerator {
 private:
     T first_;
@@ -22,13 +63,22 @@ public:
     FibonacciGenerator() : first_(0), second_(1) {}
 
     FibonacciGenerator(const T& first_0, const T& second_0) :
-        first_(first_0), second_(second_0) {}
+        first_(first_0), second_(second_0) {
+        generateNew();
+        generateNew();
+    }
 
     constexpr T operator () (){
         T tmp = first_;
         generateNew();
         return tmp;
     }
+
+    auto getIterator(){
+        return FibonacciIterator(*this);
+    }
+
+    friend class FibonacciIterator<T, Plus>;
 };
 
-#endif // FIB_GENERATOR_H
+#endif // FIBONACCI_GENERATOR_H
--- main.cpp
+++ main.cpp
@@ -8,13 +8,25 @@ using std::endl;
 using std::string;
 
 int main() {
-
+    //Create generator with the default constructor (0, 1)
     FibonacciGenerator<int> gen1 {};
     std::generate_n(std::ostream_iterator<decltype(gen1)::result_type>(cout," "), 20, gen1);
     cout << endl;
 
+//    auto gg = gen1;
+
+    //Concatenate strings in Fibonacci way
     FibonacciGenerator gen2 { string{"A"}, string{"B"} };
     std::generate_n(std::ostream_iterator<decltype(gen2)::result_type>(cout," "), 10, gen2);
+    cout << endl;
+
+    //Get the max Fibonacci number less than 1000
+    for (auto it = gen1.getIterator(); *it < 1000; ++it);
+
+    //Iterate to the same state
+    FibonacciGenerator<int> gen3 {};
+    for (auto it = gen3.getIterator(); it != gen1.getIterator(); ++it)
+        cout << *it << " ";
     cout << endl;
 
     return 0;
